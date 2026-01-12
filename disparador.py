b6b05f5e42288e960869ad8ee36784f3f9c1e6b9
RodrigoJp47 <rodriabreu@gmail.com>
2026-01-12 10:01:35 -0300

    Ajuste tempo

--- disparador.py
+++ disparador.py
@@ -8,8 +8,8 @@ from datetime import datetime
 API_KEY = "87cc26577dac7e7b62287fb2e3e54f40397395679518a15d1d731e041d00d462"
 API_URL = "https://www.wasenderapi.com/api/send-message"
 NOME_ARQUIVO = "lista_clientes.xlsx"
-TEMPO_MIN = 600
-TEMPO_MAX = 1200
+TEMPO_MIN = 400
+TEMPO_MAX = 900
 
 def enviar_disparos():
     if not os.path.exists(NOME_ARQUIVO):
